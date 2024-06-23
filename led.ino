fa57a29bf5296323cbcf95030b03589445baefdd
Bagavathi2002 <140944606+Bagavathi2002@users.noreply.github.com>
2024-06-23 19:51:28 +0530

    led.ino

--- led.ino
+++ led.ino
@@ -14,11 +14,3 @@ void setup() {
   // put your setup code here, to run once:
 pinMode(9,OUTPUT);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-analogWrite(9,255);
-delay(1000);
-analogWrite(9,4);
-delay(1000);
-}
